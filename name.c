aab591a209d968b9b2ce5f3324cc6379c76cce00
eshaandinesh <147300682+eshaandinesh@users.noreply.github.com>
2024-02-05 23:36:01 +0530

    Create name.c

---
+++ name.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+int j;
+  for(int i=1;i<=5;i++){ //initialisation of iterable;end case; unit updation;
+    printf("Eshaan\n");
+  }
+  j=1;  //steps: declare i; intialise i; check condition(happens n+1 times); print body(n times); update i(n times)
+  while(j<=5){
+    printf("Dinesh\n");
+    j++;
+  }
+}
